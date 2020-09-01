56f60dcbe6b424f4fece57fbab179d72164544f5
Mateone <54219667+Mateone@users.noreply.github.com>
2020-09-01 20:35:22 -0300

    cambios de argumentación

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -6,7 +6,8 @@ namespace SRP
     {
         //El metodo "Libro" está bien asignado ya que la clase es la experta en la información,
         //pero en mi opinión el metodo "AlmacenarLibro" debería ser responsabilidad de otra clase, para cumplir con el SRP, y a su vez cumplir con el expert.
-        // O sea, en este momento la clase tiene dos motivos por los cuales cambiar.
+        //O sea, en este momento la clase tiene dos motivos por los cuales cambiar.
+        //La responsabilidad de almacenar libros no debería ser del libro, sino de otra clase llamada "Biblioteca", por ejemplo.
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
